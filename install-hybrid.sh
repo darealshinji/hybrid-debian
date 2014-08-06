a175afe1be383ae6dc56502126ef147c56f5d072
djcj <djcj@gmx.de>
2014-08-06 05:57:50 +0200

    get newest version string from RSS feed

--- install-hybrid.sh
+++ install-hybrid.sh
@@ -3,15 +3,16 @@
 QT=$1
 
 URL=http://www.selur.de/sites/default/files/hybrid_downloads
-available_ver=$(wget -q -O - $URL/version.txt)
+#available_ver=$(wget -q -O - $URL/version.txt)
+available_ver=$(wget -q -O - http://forum.selur.de/feed-rss-topic32.xml | perl -ne 'printf qq[%s\n], $1 if m/<strong>([^<]+)<\/strong>/' | sed -e 's/\://' | head -n1 | cut -d' ' -f3)
 
 YY=$(echo $available_ver | cut -d'.' -f1 | tail -c+3)
 MM=$(echo $available_ver | cut -d'.' -f2)
 DD=$(echo $available_ver | cut -d'.' -f3)
-_REV=$(echo $available_ver | cut -d'.' -f4)
+revision=$(echo $available_ver | cut -d'.' -f4)
 
-if [ $_REV != 1 ] ; then
-   REV=_$_REV
+if [ $revision != 1 ] ; then
+   REV=$revision
 fi
 
 VER=$YY$MM$DD$REV
