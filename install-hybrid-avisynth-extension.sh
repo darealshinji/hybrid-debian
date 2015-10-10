253261555823eac63bc014c0980759f4d3ceb0fd
darealshinji <djcj@gmx.de>
2015-10-10 21:10:24 +0200

    Install avs plugins in $HOME/.hybrid-bin/avisynth

--- install-hybrid-avisynth-extension.sh
+++ install-hybrid-avisynth-extension.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-ver_url=https://raw.githubusercontent.com/darealshinji/hybrid-debian/avisynth-extension/version
-pkg_ver=$(wget -O - $ver_url)
+pkg_ver=${1}
 
 url=http://www.selur.de/sites/default/files/hybrid_downloads/avisynth
 zip=avisynthExtension_${pkg_ver}.7z
@@ -11,13 +10,15 @@ bin="$HOME/.hybrid-bin"
 wget -q --spider $url/$zip
 
 if [ $? = 0 ] ; then
-    mkdir -p "$bin"
-    rm -rf "$bin/avisynthPlugins" "$bin/dynamic" "$bin/platforms"
+    rm -rf "$bin/avisynth"
+    # old files
+    rm -rf "$bin/avisynthPlugins" "$bin/platforms"
     rm -f "$bin/*.exe" "$bin/*.dll" "$bin/AVSMeter.ini" "$bin/avisynthExtension_*.7z"
-    cd "$bin"
+    mkdir -p "$bin/avisynth"
+    cd "$bin/avisynth"
     wget $url/$zip
     7z x $zip
-    echo $pkg_ver > "$bin/avisynthPlugins/version"
+    echo $pkg_ver > "$bin/avisynth/version"
     rm -f "$bin/$zip"
 fi
 
